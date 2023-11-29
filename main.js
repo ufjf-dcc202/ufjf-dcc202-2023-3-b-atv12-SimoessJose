a97ef9e1a95405780dd0a4eb0a2c9b2bebe0c89c
José Simões <jose.simoes@estudante.ufjf.br>
2023-11-28 23:02:16 -0300

    corrige erros

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const olJoao = document.querySelector("ol#joao");
+const olMaria = document.querySelector("ol#maria");
 
 document.entrada.addEventListener('submit', leFormulario);
 
@@ -5,7 +7,10 @@ function leFormulario(event){
 
     event.preventDefault();
     const quantidade = document.entrada.quantidade.value;
+    valueAsNumber;
     const fruta = document.entrada.fruta.value;
+    const origin = document.entrada.origem.value;
+    const destino = document.entrada.destino.value;
 
-    console.log(`Eu tenho ${quantidade} ${fruta}`);
+    console.log(`${origem} doa ${quantidade} ${fruta} para ${destino}`);
 }
