707faad2e0c5f988a6f0c0bee25f55b27a9c88a4
José Simões <jose.simoes@estudante.ufjf.br>
2023-12-05 21:53:45 -0300

    refatoramento funçoes

--- main.js
+++ main.js
@@ -18,13 +18,20 @@ function leFormulario(event){
 
     function atualizaTela(){
         const estoque = getEstoque();
-        olJoao.innerHTML = "";
-        for(let i=0; i<estoque.joao.length; i++){
-            const monte = estoque.joao[i];
+        preencheLista(olJoao, estoque.joao);
+        preencheLista(olMaria, estoque.maria);
+
+    }
+
+    function preencheLista (lista, estoque) {
+        lista.innerHTML = "";
+        for(let i=0; i<estoqueDaPessoa.length; i++){
+            const monte = estoqueDaPessoa[i];
             const li = document.createElement('li');
             li.textContent = `${monte.tipo}: ${monte.qtd}`;
-            olJoao.append(li);
+            lista.append(li);
         }
 
     }
+
 }
